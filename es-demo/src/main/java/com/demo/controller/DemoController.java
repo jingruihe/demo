5b4a85c66853afbfe1c31fdf551a9511bf2bfe24
JingRui <jingruihe@outlook.com>
2021-11-07 22:15:36 +0800

    -

--- es-demo/src/main/java/com/demo/controller/DemoController.java
+++ es-demo/src/main/java/com/demo/controller/DemoController.java
@@ -4,16 +4,20 @@ import com.demo.constant.ESConstant;
 import com.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoController {
 
     @Autowired
     private UserService userService;
 
+
     @GetMapping("createIndex")
-    public void createIndex() {
+    public boolean createIndex() {
         String s = "{\n" +
                 "  \"properties\": {\n" +
                 "    \"city\": {\n" +
@@ -33,7 +37,36 @@ public class DemoController {
                 "    }\n" +
                 "  }\n" +
                 "}";
-        boolean index = userService.createIndex(ESConstant.USER_INDEX, s);
-        System.out.println(index);
+        return userService.createIndex(ESConstant.USER_INDEX, s);
+    }
+
+    @GetMapping("getIndex")
+    public String getIndex() {
+        return userService.getIndex(ESConstant.USER_INDEX);
+    }
+
+    @DeleteMapping("deleteIndex")
+    public boolean deleteIndex() {
+        return userService.deleteIndex(ESConstant.USER_INDEX);
+    }
+
+    @PostMapping("getAllDocument")
+    public String getAllDocument() {
+        return userService.getAllDocument(ESConstant.USER_INDEX);
+    }
+
+    @PostMapping("addDocument")
+    public boolean addDocument() {
+        return userService.addDocument(ESConstant.USER_INDEX);
+    }
+
+    @PostMapping("getDocument")
+    public String getDocument() {
+        return userService.getDocument(ESConstant.USER_INDEX);
+    }
+
+    @PostMapping("updateDocument")
+    public Boolean updateDocument() {
+        return userService.updateDocument(ESConstant.USER_INDEX);
     }
 }
